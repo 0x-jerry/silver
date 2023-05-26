f14fe39814a861df40cd9d4ea1a87eaa84ffc5c7
Jerry Wang <x.jerry.wang@gmail.com>
2023-05-26 09:22:00 +0800

    Initial commit

---
+++ vitest.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vitest/config'
+import GithubActionsReporter from 'vitest-github-actions-reporter'
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'node',
+    coverage: {
+      provider: 'c8',
+      include: ['src/**/*.ts'],
+      exclude: ['src/**/*.d.ts', 'src/**/*.test.ts'],
+      reporter: ['clover', 'html-spa'],
+    },
+    reporters: process.env.GITHUB_ACTIONS ? new GithubActionsReporter() : 'default',
+  },
+})
