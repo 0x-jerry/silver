75d65dabd24f9502c5caa2bc9aa2a727b59cb88d
Jerry Wang <x.jerry.wang@gmail.com>
2023-05-26 16:09:40 +0800

    feat: define type

--- src/core.ts
+++ src/core.ts
@@ -1,14 +1,19 @@
+import { Value } from '@0x-jerry/utils'
 import { parseCliProgram } from './parse'
 import { CliProgram } from './types'
 
 export class Sliver {
   conf?: CliProgram
 
+  typeMapper = new Map<string, Value<string[]>>()
+
   parse(raw: TemplateStringsArray, ...tokens: any[]) {
     this.conf = parseCliProgram(raw, ...tokens)
   }
 
-  type(name: string, getType: () => string[]) {
+  type(name: string, getType: Value<string[]>) {
+    this.typeMapper.set(name, getType)
+
     return this
   }
 }
