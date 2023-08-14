c7ae3094f3613b32f1a6aef1cbf0f7dc2cca0d51
Jerry Wang <x.jerry.wang@gmail.com>
2023-08-14 15:13:23 +0800

    chore: update dev deps

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,16 +1,14 @@
 import { defineConfig } from 'vitest/config'
-import GithubActionsReporter from 'vitest-github-actions-reporter'
 
 export default defineConfig({
   test: {
     globals: true,
     environment: 'node',
     coverage: {
-      provider: 'c8',
+      provider: 'v8',
       include: ['src/**/*.ts'],
       exclude: ['src/**/*.d.ts', 'src/**/*.test.ts'],
       reporter: ['clover', 'html-spa'],
     },
-    reporters: process.env.GITHUB_ACTIONS ? ['default', new GithubActionsReporter()] : 'default',
   },
 })
