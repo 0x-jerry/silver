9d1997ab005aeb45a881be40d61ab0bb0f025afe
Jerry Wang <x.jerry.wang@gmail.com>
2024-07-19 23:46:20 +0800

    test: remove un-supported syntax

--- test/index.test.ts
+++ test/index.test.ts
@@ -13,17 +13,16 @@ describe('silver', () => {
 commandName [dir], A library for create command line interface quickly. ${fn}
 
 -s --string @string:cool, An string option with default value.
--n --number #global @number:123.34, an number option with default value, and it's a global option.
+-n --number @number:123, an number option with default value, and it's a global option.
 -e --enum @custom:${defaultCustom}, an custom option with default value.
 
 -b --bool @bool, an boolean option without default value.
 -o --other, an option without specify a type will be a string.
 
-# aliasName/commandName
 up/upgrade <dir> [...other] #stopEarly, an sub command. ${fn}
 
 -s --string @string:default, sub command option.
--sm --small @bool:true, other option.
+--small @bool:true, other option.
 `
 
     expect(ins.conf).toMatchSnapshot()
@@ -64,17 +63,16 @@ sub, test command. ${fn}
 xx [dir], A library for create command line interface quickly. ${fn}
 
 -s --string @string:cool, An string option with default value.
--n --number #global @number:123.34, an number option with default value, and it is a global option.
+-n --number @number:123, an number option with default value, and it is a global option.
 -e --enum @custom:${defaultCustom}, an custom option with default value.
 
 -b --bool @bool, an boolean option without default value.
 -o --other, an option without specify a type will be a string.
 
-# aliasName/commandName
-up:dev/upgrade <dir> [...other] #stopEarly, an sub : command. ${fn}
+up/upgrade <dir> [...other] #stopEarly, an sub : command. ${fn}
 
 -s --string @string:default, sub command option.
--sm --small @bool, other option.
+--small @bool, other option.
 `
 
     const code = generateZshAutoCompletion(ins.conf!.command)
